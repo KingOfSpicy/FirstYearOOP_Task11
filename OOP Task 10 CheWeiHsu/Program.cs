ef4e1f69f89502a0bd71d1ceeff9bd43a46b7f5c
Käyttäjä <sangowosan@gmail.com>
2019-03-25 14:21:38 +0200

    Add project files.

---
+++ OOP Task 10 CheWeiHsu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task_10_CheWeiHsu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
