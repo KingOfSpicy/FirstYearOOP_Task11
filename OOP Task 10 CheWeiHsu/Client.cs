adbe92f2986bdc0f4d212ff89fa5e69c8a6612aa
KingOfSpicy <sangowosan@gmail.com>
2022-03-01 10:51:22 +0200

    add client class.

---
+++ OOP Task 10 CheWeiHsu/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task_10_CheWeiHsu
+{
+    class Client
+    {
+        public Client(int id, string firstName, string familyName, string telephoneNumber, string email, string preferredType, double maxRentPossible)
+        {
+            Id = id;
+            FirstName = firstName;
+            FamilyName = familyName;
+            TelephoneNumber = telephoneNumber;
+            Email = email;
+            PreferredType = preferredType;
+            MaxRentPossible = maxRentPossible;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PreferredType { get; set; }
+        public double MaxRentPossible { get; set; }
+    }
+}
